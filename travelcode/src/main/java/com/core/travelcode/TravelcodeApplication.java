a4899e266b723bfd6d1abcf22832ec5f0f949df5
stefanrosic <stefanrosic2@gmail.com>
2022-05-09 17:48:26 +0200

    initial setup commit

---
+++ travelcode/src/main/java/com/core/travelcode/TravelcodeApplication.java
@@ -0,0 +1,13 @@
+package com.core.travelcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelcodeApplication.class, args);
+	}
+
+}
---
+++ travelcode/src/test/java/com/core/travelcode/TravelcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.travelcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
